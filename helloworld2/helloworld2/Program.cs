25c69d9005d7e2369fb96d358a9fe924483a4af9
m38bob <m38bob@gmail.com>
2017-06-14 11:00:45 -0400

    Added a static text writer

--- helloworld2/helloworld2/Program.cs
+++ helloworld2/helloworld2/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace helloworld2
 {
@@ -10,7 +6,10 @@ namespace helloworld2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world!");
+            StaticTextWriter.WriteLine1();
+            StaticTextWriter.WriteLine2();
+            StaticTextWriter.WriteLine3();
+            StaticTextWriter.WriteLine4();
         }
     }
 }
---
+++ helloworld2/helloworld2/StaticTextWriter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace helloworld2
+{
+    public static class StaticTextWriter
+    {
+        public static void WriteLine1()
+        {
+            Console.WriteLine("Hello world!");
+        }
+        public static void WriteLine2()
+        {
+            Console.WriteLine("Hello world, AGAIN!");
+        }
+        public static void WriteLine3()
+        {
+            Console.WriteLine("... and AGAIN, yikes...!");
+        }
+        public static void WriteLine4()
+        {
+            Console.WriteLine("You sound unhinged");
+        }
+    }
+}
